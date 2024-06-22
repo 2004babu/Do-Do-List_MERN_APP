67e4e66b7b01026ed3fc959bc1e9a9d25ad353e5
2004babu <155835589+2004babu@users.noreply.github.com>
2024-06-22 12:07:36 +0530

    Update db.js

--- Backend/DataBase/db.js
+++ Backend/DataBase/db.js
@@ -5,8 +5,7 @@ const path=require('path')
 dotenv.config({path:path.join(__dirname,'../config/Do-Do.env')})
 const connectDB=async()=>{
     try {
-        await mongoose.connect(process.env.MONGO_URL, {useCreateIndex: true,
-            useFindAndModify: false,
+        await mongoose.connect(process.env.MONGO_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true })
         console.log(`MongoDB connected IN ${process.env.MONGO_URL}`);
