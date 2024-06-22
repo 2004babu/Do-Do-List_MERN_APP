5eb544364f7423c2bd7fd68f738cf9cb9a687da6
2004babu <155835589+2004babu@users.noreply.github.com>
2024-06-22 12:04:57 +0530

    Update db.js
    add new options ..remove deprecated options in dataBase

--- Backend/DataBase/db.js
+++ Backend/DataBase/db.js
@@ -5,7 +5,10 @@ const path=require('path')
 dotenv.config({path:path.join(__dirname,'../config/Do-Do.env')})
 const connectDB=async()=>{
     try {
-        await mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+        await mongoose.connect(process.env.MONGO_URL, {useCreateIndex: true,
+            useFindAndModify: false,
+            useNewUrlParser: true,
+            useUnifiedTopology: true })
         console.log(`MongoDB connected IN ${process.env.MONGO_URL}`);
     } catch (error) {
         console.log(`MongoDB NOt connected ,,  Error : ${error}`);
